43ec2009a9b0cb16d1b668da7cfe6058ff57eee2
Mickaël Véril <mika.veril@wanadoo.fr>
2022-07-08 10:03:20 +0200

    Initial commit

---
+++ src/main/java/org/mveril/App.java
@@ -0,0 +1,13 @@
+package org.mveril;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
